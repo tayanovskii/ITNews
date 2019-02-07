3a45298f6a9ba31d61143cf4373cd3817ef2d1dd
tayanovskii <tayanovskii@gmail.com>
2019-02-07 22:28:10 +0300

    exclude appsettings

---
+++ ITNews/ITNews/Data/Entities/NewsQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ITNews.Helpers;
+
+namespace ITNews.Data.Entities
+{
+    public class NewsQuery : IQueryObject
+    {
+        public string SortBy { get; set; }
+        public bool IsSortAscending { get; set; }
+        public int Page { get; set; }
+        public byte PageSize { get; set; }
+    }
+}
---
+++ ITNews/ITNews/Helpers/IQueryObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITNews.Helpers
+{
+    public interface IQueryObject
+    {
+        string SortBy { get; set; }
+        bool IsSortAscending { get; set; }
+        int Page { get; set; }
+        byte PageSize { get; set; }
+    }
+}
