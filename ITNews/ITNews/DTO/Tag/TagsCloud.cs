5cd844741de9ede21d28bb7bc4a9f5a1a9940742
tayanovskii <tayanovskii@gmail.com>
2019-02-19 20:46:20 +0300

    add tagsCloud action

---
+++ ITNews/ITNews/DTO/Tag/TagsCloud.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITNews.DTO.Tag
+{
+    public class TagsCloud
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int EntryCount { get; set; }
+    }
+}
--- ITNews/ITNews/Mapping/TagProfile.cs
+++ ITNews/ITNews/Mapping/TagProfile.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using ITNews.Data.Entities;
 using ITNews.DTO;
+using ITNews.DTO.Tag;
 
 namespace ITNews.Mapping
 {
@@ -13,6 +14,10 @@ namespace ITNews.Mapping
         public TagProfile()
         {
             CreateMap<Tag, TagDto>().ForMember(dto => dto.Name, opt => opt.MapFrom(tag => tag.Name)).ReverseMap();
+            CreateMap<Tag, TagsCloud>()
+                .ForMember(cloud => cloud.Id, opt=>opt.MapFrom(tag => tag.Id))
+                .ForMember(cloud => cloud.Name, opt=> opt.MapFrom(tag => tag.Name))
+                .ForAllOtherMembers(opt=>opt.Ignore());
         }
     }
 }
